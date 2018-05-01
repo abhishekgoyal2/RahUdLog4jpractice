e43699c1c903e04754a54130bbc76079b4f0d45f
abhishek.goyal2 <abhi25goel@gmail.com>
2018-05-01 14:38:37 +0530

    Log.debug

---
+++ src/alpha1/Demolog.java
@@ -0,0 +1,15 @@
+package alpha1;
+import org.apache.logging.log4j.*;
+//import java.util.logging.LogManager;
+//import java.util.logging.Logger;
+
+public class Demolog {
+
+	private static Logger log=LogManager.getLogger(Demolog.class.getName());
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+log.debug("this is debugging");
+	}
+
+}
